ef4277d19e0d71c8a92aec9d3b72640d27f280cb
Eseoghene Ighomuaye <eseigh26@gmail.com>
2022-10-26 18:26:49 +0100

    initial commit

---
+++ JS_Java.js
@@ -0,0 +1,21 @@
+// var x="ing";
+// var y="warn";
+// var result=y+x;
+// alert(result);
+
+alert("warning");
+function my_func(){
+    var x;
+    
+}
+
+// function my_func(){
+//     var x= prompt("input something:")
+//     var y="Your input:";
+//     alert(y+x);
+// }
+
+
+
+
+/*
